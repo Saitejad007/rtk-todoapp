e8375c34d3cd7328d08ba7f3b141de50fdb3ff89
Sai Teja <saitejad@mirakitech.com>
2024-01-31 19:54:30 +0530

    Added auth slice

--- src/app/lib/redux/store.ts
+++ src/app/lib/redux/store.ts
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import todoReducer from "./slices/todoSlice";
+import authReducer from "./slices/authSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({ todoReducer, authReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    todo: todoReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [...getDefaultMiddleware(), thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
